be2b7b6449b9ebda4ea70aac641d229ec6b2aac5
Divanio Silva <dcsilva@mail.ru>
2022-01-25 21:25:05 +0100

    Fix orders to customers relation

--- src/main/java/com/example/springbootgraphql/controller/CustomerController.java
+++ src/main/java/com/example/springbootgraphql/controller/CustomerController.java
@@ -19,12 +19,12 @@ public class CustomerController {
         this.customerRepository = customerRepository;
     }
 
-    @SchemaMapping(value = "Customer")
+    @SchemaMapping(typeName = "Customer")
     Flux<Order> orders(Customer customer){
         List<Order> orders = new ArrayList<>();
         for (int orderid = 1; orderid <= (Math.random() * 100); orderid++) {
-            //orders.add(Order.builder().id(orderid).customerId(customer.getId()).build());
-            orders.add(new Order(orderid, customer.getId()));
+            orders.add(Order.builder().id(orderid).customerId(customer.getId()).build());
+            //orders.add(new Order(orderid, customer.getId()));
         }
         return Flux.fromIterable(orders);
     }
